649dce59ef7c722022c6b44636c9448ad3c9aa39
oskarrough <oskar@rough.dk>
2022-10-15 12:31:12 +0200

    "Fix" more imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { supabase } from './supabase-client.js'
 import { signUp, signIn, signOut } from './auth.js'
 import { deleteUser } from './user.js'
-import { createChannel, updateChannel, deleteChannel } from './channel.js'
+import { createChannel, updateChannel, deleteChannel, findChannels } from './channel.js'
 import { createTrack, updateTrack, deleteTrack } from './track.js'
 
-export {
+const sdk = {
 	supabase,
 	signUp, signIn, signOut,
 	deleteUser,
-	createChannel, updateChannel, deleteChannel,
+	createChannel, updateChannel, deleteChannel, findChannels,
 	createTrack, updateTrack, deleteTrack
 }
 
+export default sdk
