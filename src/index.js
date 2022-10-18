0d43eb0ea7c159cd91bb9b0c95dfe88478a1aec7
Oskar Rough <oskar@rough.dk>
2022-10-18 10:51:01 +0200

    Add sdk.getUser()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { supabase } from './supabase-client.js'
 import { signUp, signIn, signOut } from './auth.js'
-import { deleteUser } from './user.js'
+import { getUser, deleteUser } from './user.js'
 import { createChannel, updateChannel, deleteChannel, findChannels, findChannelBySlug } from './channel.js'
 import { createTrack, updateTrack, deleteTrack } from './track.js'
 
 const sdk = {
 	supabase,
 	signUp, signIn, signOut,
-	deleteUser,
+	getUser, deleteUser,
 	createChannel, updateChannel, deleteChannel, findChannels, findChannelBySlug,
 	createTrack, updateTrack, deleteTrack
 }
--- src/user.js
+++ src/user.js
@@ -1,5 +1,11 @@
 import {supabase} from './supabase-client.js'
 
+export async function getUser() {
+	const {data, error} = await supabase.auth.getUser()
+	if (error) return {error}
+	return data.user
+}
+
 // Will delete the currently authenticated user's "auth user" and any "user channels".
 // The function is defined in https://github.com/internet4000/radio4000-supabase/blob/main/04-radio4000.sql
 export const deleteUser = async () => {
