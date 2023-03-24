79238dea72ff280f8a8f7abf1610231215a5a87c
oskarrough <oskar@rough.dk>
2023-03-24 13:56:04 +0100

    Replace dotenv

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,9 +1,9 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = process?.env.SUPABASE_URL || 'https://myjhnqgfwqtcicnvcwcj.supabase.co'
+const supabaseKey = process?.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTQxNTQ3MiwiZXhwIjoxOTU2OTkxNDcyfQ.gySR3Lv-m_CIj2Eyx6kfyOdwwMXEOFOgeHSjADqcM4Y'
+
+console.log(supabaseUrl)
 
 const isBrowser = typeof window !== 'undefined'
 
