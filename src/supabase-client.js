a3b705767611d657eba81b1b89b6bbef88eb397a
oskarrough <oskar@rough.dk>
2023-03-24 13:59:54 +0100

    Remove console log

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -3,8 +3,6 @@ import {createClient} from '@supabase/supabase-js'
 const supabaseUrl = process?.env.SUPABASE_URL || 'https://myjhnqgfwqtcicnvcwcj.supabase.co'
 const supabaseKey = process?.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTQxNTQ3MiwiZXhwIjoxOTU2OTkxNDcyfQ.gySR3Lv-m_CIj2Eyx6kfyOdwwMXEOFOgeHSjADqcM4Y'
 
-console.log(supabaseUrl)
-
 const isBrowser = typeof window !== 'undefined'
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
