58d1ed1e5658937d0e8600618b4b4b3b4858c983
oskarrough <oskar@rough.dk>
2023-03-25 08:06:18 +0100

    Possible fix

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,9 +1,10 @@
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseUrl = process?.env.SUPABASE_URL || 'https://myjhnqgfwqtcicnvcwcj.supabase.co'
-const supabaseKey = process?.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTQxNTQ3MiwiZXhwIjoxOTU2OTkxNDcyfQ.gySR3Lv-m_CIj2Eyx6kfyOdwwMXEOFOgeHSjADqcM4Y'
-
 const isBrowser = typeof window !== 'undefined'
+const hasEnv = typeof process !== 'undefined'
+
+const supabaseUrl = hasEnv ? process.env.SUPABASE_URL : 'https://myjhnqgfwqtcicnvcwcj.supabase.co'
+const supabaseKey = hasEnv ? process.env.SUPABASE_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTQxNTQ3MiwiZXhwIjoxOTU2OTkxNDcyfQ.gySR3Lv-m_CIj2Eyx6kfyOdwwMXEOFOgeHSjADqcM4Y'
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
 	auth: {
