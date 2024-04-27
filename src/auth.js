9720fed61ce605cadd310bbe4aab66f98e1c512a
ug.rp <ugrp@duck.com>
2024-04-27 10:08:14 +0200

    fix jsdocs types for auth.signUp options

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ import {supabase} from './main.js'
 // Three wrappers around the auth methods from Supabase.
 
 /**
- * @param {{email: string, password: string}} param0
+ * @param {{email: string, password: string, options: object}} param0
  */
 export const signUp = async ({email, password, options = {}}) => {
 	return supabase.auth.signUp({email, password, options})
