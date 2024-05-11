5d379ecee369c7ee44d3e7baf9108b7426286284
oskarrough <oskar@rough.dk>
2024-05-11 14:10:05 +0200

    Update Supabase dependency

--- src/users.js
+++ src/users.js
@@ -15,7 +15,7 @@ export async function readUser(jwtToken) {
 
 /**
  * Will delete the currently authenticated user's "auth user" and any "user channels".
- * The function is defined in https://github.com/internet4000/radio4000-supabase/blob/main/04-radio4000.sql
+ * The function is defined in https://github.com/radio4000/supabase/blob/main/04-radio4000.sql
  */
 export const deleteUser = async () => {
 	return supabase.rpc('delete_user')
