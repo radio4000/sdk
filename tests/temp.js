1baf2b3eaf99afe9e03d3568d74b2d6e7c15f3fb
oskarrough <oskar@rough.dk>
2025-11-08 19:00:17 +0100

    Lint

--- tests/temp.js
+++ tests/temp.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import {expect, test} from 'vitest'
 
 // The auth and crud test files have been moved to ./tests-disabled/,
 // because they require a DB to run. And we don't have that setup.
@@ -6,4 +6,3 @@ import { test, expect } from 'vitest'
 test('we dont have tests', async () => {
 	expect(1).toBe(1)
 })
-
