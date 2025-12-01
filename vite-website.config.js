1d1f82313a28864c0215d8ce5ae9939f18822725
Oskar <oskar@rough.dk>
2025-12-01 12:07:35 +0100

    Show npm version on docs page (#31)

--- vite-website.config.js
+++ vite-website.config.js
@@ -1,9 +1,12 @@
-import {resolve} from 'path'
 import {defineConfig} from 'vite'
+import pkg from './package.json' with {type: 'json'}
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
 		emptyOutDir: false
+	},
+	define: {
+		'import.meta.env.PKG_VERSION': JSON.stringify(pkg.version)
 	}
 })
